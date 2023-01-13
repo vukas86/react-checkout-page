3e2f71c61bdbe19ff2dc3b5051dc9b13365a1d89
“vukas86” <vukasbit@gmail.com>
2023-01-13 19:27:51 +0100

    working on UI experience

--- src/components/checkout-page/Page.jsx
+++ src/components/checkout-page/Page.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Form from "./Form.jsx";
 import CartInfo from "./CartInfo.jsx";
 import CartContext from "../../CartContext.jsx";
@@ -7,18 +7,23 @@ import styles from "./Page.module.css";
 
 function Page() {
   const [items] = useContext(CartContext);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmitt, setDidSubmitt] = useState(false);
 
-  const submitOrderHandler = (userData) => {
-    fetch(
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
       "https://smart-store-23-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
       {
         method: "POST",
         body: JSON.stringify({ user: userData, orderedItems: items }),
       }
     );
+    setIsSubmitting(false);
+    setDidSubmitt(true);
   };
 
-  return (
+  const cartModalContent = (
     <>
       <div className={styles.container}>
         <h2>Payment Information</h2>
@@ -27,6 +32,29 @@ function Page() {
           <CartInfo />
         </section>
       </div>
+    </>
+  );
+
+  const isSubmittingContent = (
+    <>
+      <div className={styles.submittContainer}>
+        <p>Sending order data, please stay on this page!</p>
+      </div>
+    </>
+  );
+  const didSubmitContent = (
+    <>
+      <div className={styles.submittContainer}>
+        <p>Your order is succesfully submitted!</p>
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      {!isSubmitting && !didSubmitt && cartModalContent}
+      {isSubmitting && isSubmittingContent}
+      {didSubmitt && didSubmitContent}
     </>
   );
 }
